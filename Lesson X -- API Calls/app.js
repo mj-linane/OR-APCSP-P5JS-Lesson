4f33890239e3ea3927c479e0d5e27f1ce953599d
MJ Linane <mj@mjlinane.com>
2020-01-07 10:55:11 -0500

    Convert to Dark Sky API

--- Lesson X -- API Calls/app.js
+++ Lesson X -- API Calls/app.js
@@ -1,18 +1,22 @@
 let weather;
+let temp;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  loadJSON('http://api.openweathermap.org/data/2.5/weather?zip=02739&units=imperial&APPID=2acad937ffe0ddfb37cf4b76eeb5d094', gotData())
-  background(0)
+  const api_url = 'https://api.darksky.net/forecast/b0ad7dfb5de5fc4b8195a1f225a19bac/41.6728825,-70.7786461'
+  const response = await fetch(api_url);
+  const json = await response.json();
+  console.log(json);
 }
 
 function gotData(data) {
   weather = data
+  console.log(weather)
 }
 function draw() {
-
   if (weather) {
+    temp = weather.currently.temperature;
+    console.log(temp);
     background()
-
   }
 }
