7591064e0bd4c36fa87de65ef975be9a2e3dcdca
MJ Linane <mj@mjlinane.com>
2020-01-16 10:37:57 -0500

    Updated lesson

--- Lesson 42 -- ID Individuals and JS Objects/app.js
+++ Lesson 42 -- ID Individuals and JS Objects/app.js
@@ -1,17 +1,23 @@
+let age = 45;
+
 const person = {
   firstName: "MJ",
   lastName: "Linane",
   age: 34,
 
   getYearBorn: function () {
-    return 2020 - this.age
+    return 2020 - this.age;
   }
 }
-console.log(person.firstName)
-let newStudent = person.firstName
-
-person.firstName = "Eli"
-console.log(person.firstName)
 
 
-console.log(person.getYearBorn());
+//console.log(person);
+
+// Access object properties
+console.log(person.firstName);
+
+person.firstName = 'Eli';
+console.log(person.firstName);
+
+let newStudent = person.firstName + " " + person.lastName
+console.log(newStudent);
