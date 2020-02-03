53f9a252dbe6c1ec30c76fc0d051acf6a13307b7
MJ Linane <mj@mjlinane.com>
2020-02-03 14:59:50 -0500

    Initial

---
+++ Lesson 5.1 -- p5js Interactivity/app.js
@@ -0,0 +1,17 @@
+let gray = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+
+function draw() {
+  background('white');
+}
+
+function mouseReleased() {
+  createP('Hello There!')
+}
+
+function createSomeText() {
+}
