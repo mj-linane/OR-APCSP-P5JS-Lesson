a91a1a85585e5250e90e4724ebc411d498bef7e2
MJ Linane <mj@mjlinane.com>
2020-02-10 10:48:56 -0500

    Uncommented

--- 5.1.add-html-dynamically/app.js
+++ 5.1.add-html-dynamically/app.js
@@ -1,25 +1,25 @@
 // CREATE SPECIFIC HTML ELEMENTS
-// function setup() {
-//   createCanvas(windowWidth, windowHeight)
-//   background('lightgray')
-//   createP('This was created in javascript!')
-// }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background('lightgray');
+  createP('This was created in javascript!');
+}
 
-// function draw() {
-//   // This will create paragraphs continually
-//   createP('This is was also created in javascript')
-// }
+function draw() {
+  // This will create paragraphs continually
+  createP('This is was also created in javascript');
+}
 
 // CREATE HTML WITH CREATEELEMENT WITHOUT IDs
-// function setup() {
-//   createCanvas(windowWidth, windowHeight)
-//   createElement('h1', 'My lucky number is...')
-//   createElement('p', '14')
-// }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  createElement('h1', 'My lucky number is...');
+  createElement('p', '14');
+}
 
 // CREATE HTML WITH CREATEELEMENT WITH IDs
-// function setup() {
-//   createCanvas(windowWidth, windowHeight)
-//   let titleText = createElement('h1', 'My lucky number is...')
-//   let numText =  createElement('p', '14')
-// }
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  let titleText = createElement('h1', 'My lucky number is...');
+  let numText = createElement('p', '14');
+}
