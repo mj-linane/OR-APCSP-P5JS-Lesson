0486158fba8956bed4ce325f95822bc82c358499
MJ Linane <mj@mjlinane.com>
2020-02-24 18:33:14 -0500

    Added Styling

--- 5.5.if-statement-and-clicker-game/banana-clicker-game-v2-solution/start.js
+++ 5.5.if-statement-and-clicker-game/banana-clicker-game-v2-solution/start.js
@@ -2,9 +2,20 @@ let startButton;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  createElement('h1', 'Welcome To The Banana Clicker Game!')
-  button = createButton('Start');
-  button.position(width / 2, height / 2);
+
+  // Create and Style Header
+  const heading = createElement('h1', 'Welcome to Catch The Banana Game!')
+  heading.style('position: absolute')
+  heading.style('width: 100%')
+  heading.style('text-align: center')
+  heading.style('top: 100px')
+  heading.style('font-family: roboto')
+
+  // Create and Style Button
+  button = createButton('START');
+  button.style('background-color: lightblue')
+  button.size(100, 50)
+  button.style('position: absolute')
   button.mousePressed(startGame);
   background(220);
 }
