1f7f2244c10c077c189f6b05ec3a9f89e68d2522
MJ Linane <mj@mjlinane.com>
2020-02-24 16:55:11 -0500

    Changed variable scope

--- 5.5.if-statement-and-clicker-game/banana-clicker-game-v2-solution/win.js
+++ 5.5.if-statement-and-clicker-game/banana-clicker-game-v2-solution/win.js
@@ -1,18 +1,16 @@
-// OPTIONAL: Retrieve game save data from local storage
-let score = sessionStorage.getItem('score')
-
-// Initialize Variables
-let resultText
-let playAgainBtn
-
 function setup() {
   createCanvas(windowWidth, windowHeight)
   background('lightgreen')
-  resultText = createElement('h1', 'You Lost :( ' + score)
+  const resultText = createElement('h1', 'You Won :)')
   resultText.position(width / 2.25, height / 2.5)
-  playAgainBtn = createButton('Play Again?')
-  playAgainBtn.position(width / 2.15, height / 2)
+  const playAgainBtn = createButton('Play Again?')
+  playAgainBtn.position(width / 2.15, height / 1.5)
   playAgainBtn.mousePressed(resetGameLink)
+
+  // OPTIONAL: Retrieve game save data from local storage
+  const score = sessionStorage.getItem('score')
+  const scoreDisplay = createElement('h2', 'Final Score: ' + score)
+  scoreDisplay.position(width / 2.25, height / 2)
 }
 
 function resetGameLink() {
