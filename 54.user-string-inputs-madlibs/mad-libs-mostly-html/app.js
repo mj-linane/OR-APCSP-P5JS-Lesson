7cb646ea1aef7c295b5a929561da00852f0c8bd6
MJ Linane <mj@mjlinane.com>
2020-02-26 10:08:20 -0500

    Initial

---
+++ 54.user-string-inputs-madlibs/mad-libs-mostly-html/app.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  background('lightgray')
+
+  const animalUI = select('#animal').value()
+  const placeUI = select('#place').value()
+
+  const outputDivUI = select('#output')
+  const inputsDivUI = select('#inputs')
+  const submitBtn = select('#submitBtn')
+
+  const animalOutputText = "I had an animal named " + animalUI + ' .It was a disguisting animal'
+  const placeOutputText = "I once lived in " + placeUI + ' . it was a tech paradise.'
+
+  submitBtn.mousePressed(displayOutput)
+
+  function displayOutput() {
+    inputsDivUI.hide()
+
+    const animalOutput = createElement('p', animalOutputText)
+    outputDivUI.appendChild(animalOutput)
+
+    const placeOutput = createElement('p', placeOutputText)
+    output  `DivUI.html(placeOutput)
+  }
+}
