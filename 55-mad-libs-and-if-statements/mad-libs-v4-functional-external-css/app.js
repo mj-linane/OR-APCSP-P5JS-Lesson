512ea9be7ce915435f8fb59add18059dd1cb0c58
MJ Linane <mj@mjlinane.com>
2020-03-02 10:34:03 -0500

    Disabled slides settings

--- 55-mad-libs-and-if-statements/mad-libs-v4-functional-external-css/app.js
+++ 55-mad-libs-and-if-statements/mad-libs-v4-functional-external-css/app.js
@@ -5,31 +5,36 @@ function setup() {
   createCanvas(windowWidth, windowHeight)
   background('lightgray')
 
+  addInputOutputElements()
+
+  addTopic(
+    'place',
+    'I once lived with my pet in a place called ',
+    '. Never have I been to a more disgusting place where tyrannical gnomes rule.',
+    false,
+  )
+
+  // Creates next button and styles it
+  const outputDisplayBtn = createButton('NEXT')
+    .mousePressed(showOutputHandler)
+    .parent(inputDivUI)
+
+  function showOutputHandler() {
+    outputDivUI.show()
+    inputDivUI.hide()
+  }
+}
+
+function addInputOutputElements() {
   createElement('h1', 'Mad Libs Game')
   createElement('h4', 'Enter Some Text to See The Result')
 
   inputDivUI = createDiv('').id('inputs')
   outputDivUI = createDiv('').id('outputs')
   outputDivUI.hide()
-
-  addTopic(
-    'place',
-    'I once lived with my pet in a place called ',
-    '. Never have I been to a more disgusting place where tyrannical gnomes rule.',
-  )
-
-  // Creates next button and styles it
-  const outputDisplayBtn = createButton('NEXT').parent(inputDivUI)
-  outputDisplayBtn.mousePressed(showOutputHandler)
-
 }
 
-function showOutputHandler() {
-  outputDivUI.show()
-  inputDivUI.hide()
-}
-
-function addTopic(topic, intro, detail) {
+function addTopic(topic, intro, detail, uppercase) {
   createElement('p', `Name a ${topic.toUpperCase()}`)
     .id('input-label')
     .parent(inputDivUI)
@@ -38,10 +43,17 @@ function addTopic(topic, intro, detail) {
     .id('input-field')
     .parent(inputDivUI)
 
-  function updateOutputHandler() {
-    const output = createP(' ').parent(outputDivUI)
-    output.html(intro + inputFieldUI.value() + detail)
-  }
-
   inputFieldUI.changed(updateOutputHandler)
+
+
+  function updateOutputHandler() {
+    const output = createP('').parent(outputDivUI)
+    let userInput = inputFieldUI.value()
+
+    if (uppercase === true) {
+      userInput = userInput.toUpperCase()
+    }
+
+    output.html(`{intro} + ${userInput} ${detail}`)
+  }
 }
