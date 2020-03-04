6d20aea6ecc5d9a57a00334ec34e29ef93a6bef0
MJ Linane <mj@mjlinane.com>
2020-03-04 14:39:47 -0500

    Added main functionality

--- 57-if-else-statements/3-dice-roller-dropdown-menu/app.js
+++ 57-if-else-statements/3-dice-roller-dropdown-menu/app.js
@@ -1,39 +1,51 @@
+let smileImg
+let sadImg
+let score = 0
+let scoreLabel
+let result
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
+
+  scoreLabel = createElement('h4', `Your Score: ${score}`).parent('inputs')
+  smileImg = createImg('', 'smile image').parent('inputs')
+  sadImg = createImg('', 'sad image').parent('inputs')
+
+  result = createP('Result')
+    .parent('inputs')
+    .id('result-text')
 
   // Create Header
   createDiv().id('inputs')
   createElement('h2', 'Game Age Checker').parent('inputs')
   createElement('h4', 'Select Your Age').parent('inputs')
-  // Create Select Button
+
+  // Create Roll Button
+  createButton('Roll the Die')
+    .parent('inputs')
+    .mousePressed(updateResultHandler)
+
+  // Create Select Dropdown
   const sel = createSelect()
   sel.parent('inputs')
-  // ADD CODE: Add dropdown options for ages 5-18+
+  sel.option('1')
+  sel.option('2')
+  sel.option('3')
+  sel.option('4')
   sel.option('5')
   sel.option('6')
-  sel.option('7')
-  sel.option('8')
-  sel.option('9')
-  sel.option('10')
-  sel.option('11')
-  sel.option('12')
-  sel.option('13')
-  sel.option('14')
-  sel.option('15')
-  sel.option('16')
-  sel.option('17')
-  sel.option('+18')
   sel.changed(updateResultHandler)
 
-  const result = createP('Result').parent('inputs').id('result-text')
+  createElement('h4', 'You rolled a: ')
 
   function updateResultHandler() {
-    // ADD CODE: Add If-Else Statement compare the age in the dropdown menu to the right age
+    resetUI()
+  }
 
-    if (sel.value() >= 13) {
-      result.html('You are old enough to play this game')
-    } else {
-      result.html('You are not old enough to play this game.')
-    }
+  function resetUI() {
+    sadImg.hide()
+    smileImg.hide()
+    score = 0
+    scoreLabel.html(`Your Score: ${score}`)
   }
 }
