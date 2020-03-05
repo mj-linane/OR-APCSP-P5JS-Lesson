2167c5dd905a38d52c31956cccb00ef29d914f0d
MJ Linane <mj@mjlinane.com>
2020-03-05 14:59:45 -0500

    Added main functionality to app

--- 57-if-else-statements/4-weekend-checker-dropdown-menu/app.js
+++ 57-if-else-statements/4-weekend-checker-dropdown-menu/app.js
@@ -1,39 +1,38 @@
+/* eslint-disable no-lonely-if */
 function setup() {
   createCanvas(windowWidth, windowHeight)
 
   // Create Header
   createDiv().id('inputs')
-  createElement('h2', 'Game Age Checker').parent('inputs')
-  createElement('h4', 'Select Your Age').parent('inputs')
-  // Create Select Button
-  const sel = createSelect()
-  sel.parent('inputs')
-  // ADD CODE: Add dropdown options for ages 5-18+
-  sel.option('5')
-  sel.option('6')
-  sel.option('7')
-  sel.option('8')
-  sel.option('9')
-  sel.option('10')
-  sel.option('11')
-  sel.option('12')
-  sel.option('13')
-  sel.option('14')
-  sel.option('15')
-  sel.option('16')
-  sel.option('17')
-  sel.option('+18')
-  sel.changed(updateResultHandler)
+  createElement('h2', 'Weekend Checker').parent('inputs')
+  createElement('h4', 'What Should I Do Today?').parent('inputs')
 
-  const result = createP('Result').parent('inputs').id('result-text')
+  // Create Day Select Button
+  const day = createSelect()
+  day.parent('inputs')
+  day.changed(updateResultHandler)
+  // ADD CODE: Add dropdown options for days of the week
+  day.option('Monday')
+  day.option('Tuesday')
+  day.option('Wednesday')
+  day.option('Thursday')
+  day.option('Friday')
+  day.option('Saturday')
+  day.option('Sunday')
+
+  const result = createP('Result')
+    .id('result-text')
+    .parent('inputs')
 
   function updateResultHandler() {
-    // ADD CODE: Add If-Else Statement compare the age in the dropdown menu to the right age
-
-    if (sel.value() >= 13) {
-      result.html('You are old enough to play this game')
+    if (day.value() === 'Saturday') {
+      result.html("It's the weekend! Stay home!")
     } else {
-      result.html('You are not old enough to play this game.')
+      if (day.value() === 'Sunday') {
+        result.html("It's the weekend! Stay home!")
+      } else {
+        result.html("It's a weekday. Go to school!")
+      }
     }
   }
 }
