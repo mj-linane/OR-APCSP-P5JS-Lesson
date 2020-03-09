f4d48227b9903304bcca3774cdbf18e38166e751
MJ Linane <mj@mjlinane.com>
2020-03-09 15:22:32 -0400

    Initial

---
+++ 57-if-else-statements/dropdown-menu-demo/if-else demo.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+
+// IF ELSE AND TYPE CONVERSIONS
+
+// TYPE CONVERSION OPTION #1
+let stringNum = Number('3')
+stringNum = Number(input.value())
+
+// TYPE CONVERSION OPTION #2
+stringNum = '3'
+const numNum = Number(stringNum)
+
+// TYPE CONVERSION OPTION #3
+if (Number(stringNum) === 3) {
+  // some code
+}
+
+// IF ELSE CONDITIONAL
+if (stringNum === 3) {
+  // some code
+} else {
+  // something else
+}
