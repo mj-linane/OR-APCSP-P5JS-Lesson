710efe68af323a2c4f4bc570a75bfdeba48afc26
MJ Linane <mj@mjlinane.com>
2020-03-23 15:10:35 -0400

    Remove draw, as while loop needs to run once

--- 60.while-loops/60.1.console-log-loop/studentStarter.js
+++ 60.while-loops/60.1.console-log-loop/studentStarter.js
@@ -3,9 +3,7 @@ let num = 0
 function setup() {
   createCanvas(windowWidth, windowHeight)
   background('white')
-}
 
-function draw() {
   while (num < 10) {
     const randomX = random(width)
     const randomY = random(height * 0.8)
@@ -14,6 +12,5 @@ function draw() {
     circle(0, 0, 20)
     translate(-randomX, -randomY)
     num = Math.floor(random(20)) // We have to use Math.floor to get an integer
-    console.log(num)
   }
 }
