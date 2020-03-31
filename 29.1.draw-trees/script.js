34a5e6d8f7617b7d2b6397b20fd2f00ae7c6aacf
MJ Linane <mj@mjlinane.com>
2020-03-31 15:38:33 -0400

    Initial

---
+++ 29.1.draw-trees/script.js
@@ -0,0 +1,52 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+}
+
+function draw() {
+  background(200)
+  console.log(mouseX, mouseY)
+  fill('SaddleBrown')
+  rect(-1, 300, 2000, 500)
+  drawtree()
+  translate(300, -134)
+  drawtree()
+  translate(300, -134)
+  drawtree()
+}
+
+function drawtree() {
+  fill('Brown')
+  square(100, 250, 50)
+  fill('green')
+  triangle(50, 250, 200, 250, 125, 75)
+  triangle(50, 200, 200, 200, 125, 50)
+  decorations()
+}
+
+function ornament() {
+  fill('red')
+  circle(100, 100, 20)
+}
+
+function decorations() {
+  ornament()
+  translate(50, 20)
+  ornament()
+  translate(20, 40)
+  ornament()
+  translate(-50, -10)
+  ornament()
+  translate(-50, 20)
+  ornament()
+  translate(60, 20)
+  ornament()
+  translate(50, 20)
+  ornament()
+  translate(-30, 20)
+  ornament()
+  translate(-50, 5)
+  ornament()
+}
+function star() {
+
+}
