1db143f57146caea2052bc277e6c557f67d40a7c
MJ Linane <mj@mjlinane.com>
2020-05-14 16:19:55 -0400

    Rename

--- 61.while-loop-simulations/solution-vanilla.js
+++ 61.while-loop-simulations/solution-vanilla.js
@@ -1,14 +1,11 @@
 let simNum = 0 // Track number of simulations run so far
 let numHeads = 0 // Track heads streak (number of heads flipped in a row)
 
-function setup() {
-  flipCounter(5)
-}
-
+flipCounter(5)
 
 function flipCounter(target) {
   while (numHeads < target) { // Change looping condition
-    const flip = Math.floor(random(2))
+    const flip = Math.floor(Math.random())
     if (flip === 1) {
       numHeads += 1 // If flip is 1 (heads), increment heads streak counter
     } else { // If flip is 0 (tails)
